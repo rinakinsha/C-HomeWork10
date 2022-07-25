49fdd9fe2cda7d2abfb15a1aa644c35af36e9e28
iryna <rinakinsha@yndex.by>
2022-07-25 22:16:51 +0300

    Editing the solution task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int StartsWithVowels(string[] words)
+{
+    char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+    int count = 0;
+
+    for (int i = 0; i < words.Length; i++)
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (words[i].ToLower()[0] == Char.ToLower(vowels[j])) count++;
+        }
+    return count;
+}
+
+string[] sadness = { "I", "don,t", "to", "say", "goodbye", ":(" };
+
+Console.Write("Number of words that start with a vowel: " + StartsWithVowels(sadness));
